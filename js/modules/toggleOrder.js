4f87f0b2ae4d3f583c0e5c4ad271794fbb5b46d0
vanyadr <vanyadr@yandex.ru>
2025-05-31 15:23:58 +0300

    order page with control functions

---
+++ js/modules/toggleOrder.js
@@ -0,0 +1,37 @@
+export const orderType = function () {
+    const personTypeBtns = document.querySelectorAll('.order__person-type-btn');
+    const deliveryTypeBtns = document.querySelectorAll('input[name="delivery"]');
+
+    const personDataForms = document.querySelectorAll('.order__person-data-form');
+    const deliveryForms = document.querySelectorAll('.order__delivery-form');
+    const payForms = document.querySelectorAll('.order__option-holder');
+
+    personTypeBtns.forEach((btn) => {
+        btn.addEventListener('click', function () {
+            const clickedBtn = this;
+
+            personTypeBtns.forEach((btn) => {btn.classList.toggle('active')});
+            personDataForms.forEach((form) => {form.classList.toggle('hide')});
+            payForms.forEach((form) => {form.classList.toggle('hide')});
+            
+            if (clickedBtn.classList.contains('order__ur')) {
+                const match = document.querySelector('input[name="order__address-match"]');
+                const factAddress = document.querySelector('input[name="fact-address"]');
+
+                match.addEventListener('click', function () {
+                    if (!match.checked) {
+                        factAddress.disabled = false;
+                    } else {
+                        factAddress.disabled = true;
+                    }
+                });
+            };
+        });
+    });
+    
+    deliveryTypeBtns.forEach((btn) => {
+        btn.addEventListener('click', function () {
+            deliveryForms.forEach((form) => {form.classList.toggle('hide')});
+        });
+    });
+};
